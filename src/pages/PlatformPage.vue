eae7b8ab33d289539fa358a892554aacee2a49fd
lcy <lcy683106@gmail.com>
2025-07-06 00:56:20 +0800

    PlatformPage.vue挂载时只加载二维的矢量和栅格地图，不再加载三维场景。三维场景和其所附带的功能将作为一个单独的模块进行呈现。

--- src/pages/PlatformPage.vue
+++ src/pages/PlatformPage.vue
@@ -2,12 +2,7 @@
     <div class="platform-page">
 
       <!-- 主窗口，动态加载当前地图组件 -->
-      <div class="main-container">
-        <RasterMapViewer v-if="currentMap === 'raster'"/>
-        <SceneViewer3D v-else-if="currentMap === 'scene3D'"/>
-        <VectorMapViewer v-else-if="currentMap === 'vector'"/>
-<!--        <SceneViewer3D/>-->
-<!--        <RasterMapViewer/>-->
+      <div class="main-container" id="main-container">
       </div>
 
       <!-- 上层的横幅和按钮 -->
@@ -17,20 +12,67 @@
 
 <script setup>
   import BannerBar from "@/components/BannerBar.vue";
-  import SceneViewer3D from "@/components/SceneViewer3D.vue";
-  import RasterMapViewer from "@/components/RasterMapViewer.vue";
   import {onMounted, ref} from "vue";
-  import VectorMapViewer from "@/components/VectorMapViewer.vue";
 
-  // 默认地图为栅格
-   const currentMap = ref('raster');
+  const SuperMap3D = window.SuperMap3D;
+  let viewer;
+
+  // 图层
+  let rasterImagery;
+  let labelImagery;
+  let vectorImagery;
+
+  // 用于控制地图图层显示或隐藏的变量
+  let showRaster = ref(true);
+  let showLabel = ref(true);
+  let showVector = ref(false);
+
+  onMounted(()=>{
+      viewer = new SuperMap3D.Viewer('main-container', {
+      useWebGPU: true,
+    });
+    window.viewer = viewer;
+
+    // 添加天地图栅格影像
+    rasterImagery = viewer.imageryLayers.addImageryProvider(new SuperMap3D.TiandituImageryProvider({
+      mapStyle: SuperMap3D.TiandituMapsStyle.IMG_C,
+      token: 'e1e9885949008a7d38002cf78ac67918',
+    }));
+
+    // 添加天地图矢量地图
+    vectorImagery = viewer.imageryLayers.addImageryProvider(new SuperMap3D.TiandituImageryProvider({
+      mapStyle: SuperMap3D.TiandituMapsStyle.VEC_C,
+      token: 'e1e9885949008a7d38002cf78ac67918'
+    }));
+
+    // 添加中文注记
+    labelImagery = viewer.imageryLayers.addImageryProvider(new SuperMap3D.TiandituImageryProvider({
+      mapStyle: SuperMap3D.TiandituMapsStyle.CTA_C,
+      token: 'e1e9885949008a7d38002cf78ac67918'
+    }));
+
+    // 影像加载后设置视角（跳转到武汉）
+    viewer.scene.camera.flyTo({
+      destination: SuperMap3D.Cartesian3.fromDegrees(114.3, 30.7, 300000), // 武汉市中心，海拔约20km
+      orientation: {
+        heading: SuperMap3D.Math.toRadians(0.0),    // 朝北
+        pitch: SuperMap3D.Math.toRadians(-90.0),    // 向下俯视
+        roll: 0.0
+      }
+    });
+
+    // 刚挂载时，默认加载栅格影像和文字注记
+    rasterImagery.show = showRaster.value;
+    labelImagery.show = showLabel.value;
+    vectorImagery.show = showVector.value;
+
+  })
 
   function handleSelectMap(type) {
-     currentMap.value = type;
-     console.log(type)
+    console.log(type)
+    rasterImagery.show = (type === "raster");
+    vectorImagery.show = (type === "vector");
   }
-
-
 
 </script>
 
