7fe9e0399a0a9eb49770a4c1ef57c4418f8a523d
lcy <lcy683106@gmail.com>
2025-07-16 17:49:18 +0800

    PlatformPage.vue挂载时加载三维场景。BannerBar.vue只显示文字横幅。SceneViewer3D.vue用加载三维场景。ViewerControl.vue用于控制视角。LayerControl.vue用于控制图层是否加载。 2025/7/76.完善了图层管理中建筑物，水体，公路，铁路的显隐控制

---
+++ src/components/TimeDisplay.vue
@@ -0,0 +1,36 @@
+<script setup>
+  import {onMounted, ref} from "vue";
+
+  let currentTime = ref("")
+
+  function updateTime() {
+    const now = new Date();
+    currentTime.value = now.toLocaleString();
+  }
+
+  onMounted(() => {
+    updateTime();
+    setInterval(updateTime, 1000); // 每秒更新
+  });
+</script>
+
+<template>
+  <div class="time-display">
+    {{currentTime}}
+  </div>
+</template>
+
+<style scoped>
+  .time-display {
+    position: absolute;
+    top: 65px;
+    left: 20px;
+    font-size: 16px;
+    background-color: rgba(5, 10, 25, 0.6);;
+    padding: 6px 12px;
+    border-radius: 8px;
+    color: #f4f0f0;
+    z-index: 9999;
+    user-select: none;
+  }
+</style>
