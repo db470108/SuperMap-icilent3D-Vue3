d2120abc780c83f76fdd943d8cabd470d50308b6
lcy <lcy683106@gmail.com>
2025-07-18 22:32:13 +0800

    PlatformPage.vue挂载时加载三维场景。BannerBar.vue只显示文字横幅。SceneViewer3D.vue用加载三维场景。ViewerControl.vue用于控制视角。LayerControl.vue用于控制图层是否加载。 2025/7/16.完善了图层管理中建筑物，水体，公路，铁路的显隐控制 2025/7/17.添加了图层控制，实现了对天空的开启和关闭，白天、夜晚和天气模式的切换 2025/7/18.对按钮和列表的样式进行了美化

--- src/components/TimeDisplay.vue
+++ src/components/TimeDisplay.vue
@@ -23,14 +23,21 @@
 <style scoped>
   .time-display {
     position: absolute;
-    top: 65px;
-    left: 20px;
+    bottom: 20px;
+    right: 20px;
     font-size: 14px;
-    background-color: rgba(5, 10, 25, 0.6);;
+    background-color: rgba(5, 10, 25, 0.6);
     padding: 6px 12px;
     border-radius: 8px;
     color: #f4f0f0;
     z-index: 9999;
     user-select: none;
+    animation: glow 2s ease-in-out infinite;
+  }
+
+  @keyframes glow {
+    0% { text-shadow: 0 0 5px #00e6ff; }
+    50% { text-shadow: 0 0 15px #00e6ff; }
+    100% { text-shadow: 0 0 5px #00e6ff; }
   }
 </style>
